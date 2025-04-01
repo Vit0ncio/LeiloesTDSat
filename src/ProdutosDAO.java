afad6a00b897a645f32ac12ad3a09ce2326f2b59
Vitor Luis <vitorluisgomesvieira0@gmail.com>
2025-04-01 12:05:24 -0300

    Funcionalidade Salvar funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,33 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
     Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public void cadastrarProduto(ProdutosDTO produto) {
+        conn = new conectaDAO().connectDB();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            String sql = "insert into produtos (nome, valor, status) values (?, ?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getValor());
+            stmt.setString(3, produto.getStatus());
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+        } catch (SQLException sqle) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + sqle.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
         return listagem;
     }
-    
-    
-    
-        
 }
-
